dfaf59d43366e660235ac367935b74f16a72f8bb
Christine Kefalianou <45734845+ckefalianou@users.noreply.github.com>
2025-05-08 22:40:33 +0300

    deleted script check sail

--- check_sail.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Check if Sail is running by looking for 'laravel.test' in Docker containers
-if [ -z "$(docker ps --format '{{.Names}}')" ]; then
-    echo "Sail is not running. Starting Sail..."
-    ./vendor/bin/sail up -d
-else
-    echo "Sail is already running."
-fi
